ac98120ee302691a1a5410f4822bb46f9162eee8
Henar Muñoz Frutos <henar.munozfrutos@telefonica.com>
2015-06-30 10:57:44 +0200

    change recipe

--- recipes/0.13.0_install.rb
+++ recipes/0.13.0_install.rb
@@ -1,2 +1,33 @@
 include_recipe "orion::mongod2"
-include_recipe "orion-dbcluster::0.13.0_install"
+
+include_recipe "orion::fiware_repo"
+
+package node[:orion][:package_name] do
+  action :install
+end
+
+
+
+
+# context init
+template "/opt/contextini.sh" do
+    action :create
+    source "contextbroker.init.erb"
+    group "root"
+    owner "root"
+    mode "777"
+end
+
+bash "install_something" do
+  user "root"
+  code <<-EOH
+    ./opt/contextini.sh > /opt/contextbroker.out
+  EOH
+end
+
+bash "start_context_broker" do
+  user "root"
+  code <<-EOH
+    /etc/init.d/contextBroker start
+  EOH
+end
